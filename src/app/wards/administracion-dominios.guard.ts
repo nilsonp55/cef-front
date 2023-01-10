4a06cc881ec10ce334d06d3d48bd9d2746a655d5
prv_jmazo <prv_jmazo@ath.com.co>
2023-01-10 16:26:12 -0500

    merge

--- src/app/wards/administracion-dominios.guard.ts
+++ src/app/wards/administracion-dominios.guard.ts
@@ -30,7 +30,7 @@ export class AdministracionDominiosGuard implements CanActivate {
         this.rolMenuService.obtenerMenuRol({
           'rol.idRol': rol,
           'estado': "1",
-          'menu.idMenuPadre': "liquidacion"
+          'menu.idMenuPadre': "administracion"
         }).subscribe(menusrol => {
           this.menus = menusrol;
           menusrol.data.forEach(itm => {
@@ -47,7 +47,7 @@ export class AdministracionDominiosGuard implements CanActivate {
           if (guardOk) {
             resolve(true);
           } else {
-            this.router.navigate(['/liquidacion']);
+            this.router.navigate(['/administracion']);
             resolve(false);
           }
         });
