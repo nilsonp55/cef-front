1cfd141252d11300b34aab1045f5e2a603c1543f
Nilson Parra <prv_nparra@ath.com.co>
2023-05-10 11:29:11 -0500

    clean alert

--- src/app/pages/modules/liquidacion/puntos-codigo-tdv/puntos-codigo-tdv.component.ts
+++ src/app/pages/modules/liquidacion/puntos-codigo-tdv/puntos-codigo-tdv.component.ts
@@ -230,20 +230,25 @@ export class PuntosCodigoTdvComponent implements OnInit {
   }
 
   async filtrarPuntos(event: any) {
-    debugger;
-    let params = {codigoCiudad: event.value ? this.ciudadSelected.codigoDANE : '', 
-      tipoPunto: this.selectedTipoPunto};
+    let params = {
+      codigoCiudad: event.value ? this.ciudadSelected.codigoDANE : '', 
+      tipoPunto: this.selectedTipoPunto
+    };
     this.gestionPuntosService.listarPuntosCreados(params).subscribe({
       next: response => {
         this.puntos = response.data.content;
       },
-      error: err => {}
+      error: err => {
+        const alert = this.dialog.open(VentanaEmergenteResponseComponent, {
+          width: GENERALES.MESSAGE_ALERT.SIZE_WINDOWS_ALERT,
+          data: {
+            msn: err.error.response.description,
+            codigo: GENERALES.CODE_EMERGENT.ERROR
+          }
+        }); 
+        setTimeout(() => { alert.close() }, 3000);
+      }
     });
-    alert("event: "+event);
-  }
-
-  async changeCiudad(event: any) {
-
   }
 
   /**
