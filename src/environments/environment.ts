489cae789f114060680fbe90e4cf5a9dcc4eedc9
Nilson Parra <prv_nparra@ath.com.co>
2023-07-21 11:43:06 -0500

    APCOR-968: agregar filtro EstadoConciliacion en Modificar operaciones

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 
 export const environment = {
   production: false,
+  appVersion: require('../../package.json').version,
   HOST: 'http://localhost:8080',
   RUTA_AUTHENTICATION: 'https://awue1athcef-pt-admin.auth.us-east-1.amazoncognito.com/oauth2/authorize?response_type=token&client_id=7lqfnlbaj4afqhq6c6rmbrde1b&redirect_uri=https://cefwebpt.aws.ath.com.co/home&scope=openid'
 };
