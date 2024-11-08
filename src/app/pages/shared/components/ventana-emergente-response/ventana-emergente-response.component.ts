13719cf50f69c54eba42a0f0786044b6a4688e70
Nilson Parra <prv_nparra@ath.com.co>
2024-11-08 16:18:14 -0500

    APCOR-2304

--- src/app/pages/shared/components/ventana-emergente-response/ventana-emergente-response.component.ts
+++ src/app/pages/shared/components/ventana-emergente-response/ventana-emergente-response.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-ventana-emergente-response',
